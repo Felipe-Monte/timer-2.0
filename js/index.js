72fb6cd911d7bdd4c895b6b9037b80d17ee7dc84
Carlos Felipe <109633306+Felipe-Monte@users.noreply.github.com>
2023-04-01 17:45:02 -0300

    new js arquive

--- js/index.js
+++ js/index.js
@@ -4,6 +4,11 @@ import sounds from "./sounds.js"
 const buttonPlay = document.querySelector('#play')
 const buttonStop = document.querySelector('#stop')
 
+const boxSoundForest = document.querySelector('#card-forest')
+const boxSoundRain = document.querySelector('#card-rain')
+const boxSoundMarket = document.querySelector('#card-market')
+const boxSoundFire = document.querySelector('#card-fire')
+
 let minutesDisplay = document.querySelector('#minutes')
 let secondsDisplay = document.querySelector('#seconds')
 
@@ -31,6 +36,15 @@ function countdown() {
   }, 100)
 }
 
+function checkColorIsBlue(item) {
+  if (item.style.backgroundColor == "rgb(2, 121, 157)" /*blue*/) {
+    item.style.backgroundColor = 'rgb(225, 225, 230' /*gray*/
+  } else {
+    item.style.backgroundColor = "rgb(2, 121, 157)"
+  }
+  return item.style.backgroundColor
+}
+
 buttonPlay.addEventListener('click', function () {
   clearInterval(resetTimer)
   countdown()
@@ -38,6 +52,47 @@ buttonPlay.addEventListener('click', function () {
 
 buttonStop.addEventListener('click', function () {
   clearInterval(resetTimer)
+  sound.stopAllSounds()
   minutesDisplay.textContent = 15
   secondsDisplay.textContent = "00"
+})
+
+boxSoundForest.addEventListener('click', function () {
+  let result = checkColorIsBlue(boxSoundForest)
+  if (result == "rgb(225, 225, 230)") {
+    sound.stopAllSounds()
+  } else {
+    sound.stopAllSounds()
+    sound.forestSound.play()
+  }
+})
+
+boxSoundRain.addEventListener('click', function () {
+  let result = checkColorIsBlue(boxSoundRain)
+  if (result == "rgb(225, 225, 230)") {
+    sound.stopAllSounds()
+  } else {
+    sound.stopAllSounds()
+    sound.rainSound.play()
+  }
+})
+
+boxSoundMarket.addEventListener('click', function () {
+  let result = checkColorIsBlue(boxSoundMarket)
+  if (result == "rgb(225, 225, 230)") {
+    sound.stopAllSounds()
+  } else {
+    sound.stopAllSounds()
+    sound.coffeeSound.play()
+  }
+})
+
+boxSoundFire.addEventListener('click', function () {
+  let result = checkColorIsBlue(boxSoundFire)
+  if (result == "rgb(225, 225, 230)") {
+    sound.stopAllSounds()
+  } else {
+    sound.stopAllSounds()
+    sound.firePlaceSound.play()
+  }
 })
--- js/sounds.js
+++ js/sounds.js
@@ -1,15 +1,23 @@
 
 export default function () {
 
-const forestSound = new Audio("./sounds/Forest.wav")
-const rainSound = new Audio("./sounds/Rain.wav")
-const coffeeSound = new Audio("./sounds/Coffee.wav")
-const firePlaceSound = new Audio("./sounds/Fireplace.wav")
+  const forestSound = new Audio("./sounds/Forest.wav")
+  const rainSound = new Audio("./sounds/Rain.wav")
+  const coffeeSound = new Audio("./sounds/Coffee.wav")
+  const firePlaceSound = new Audio("./sounds/Fireplace.wav")
+
+  function stopAllSounds() {
+    forestSound.pause()
+    rainSound.pause()
+    coffeeSound.pause()
+    firePlaceSound.pause()
+  }
 
   return {
     forestSound,
     rainSound,
     coffeeSound,
-    firePlaceSound
+    firePlaceSound,
+    stopAllSounds
   }
 }
